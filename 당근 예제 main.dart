84c7d259093a470282427b0637763b294c6848c2
tarrypotter <109096999+tarrypotter@users.noreply.github.com>
2023-06-19 02:59:49 +0900

    Update and rename dg_firstpage_main.dart to 당근 예제 main.dart

---
+++ 당근 예제 main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// MaterialApp로 시작함.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
